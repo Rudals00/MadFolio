0d172e671636255c9b197ba96198a8bada48b044
2019041703 <rudals9686@naver.com>
2023-07-14 15:35:44 +0900

    signup ing

--- my-app/src/components/signup.js
+++ my-app/src/components/signup.js
@@ -1,10 +1,34 @@
-import React, { useState } from 'react';
-import '../App.css';
+import React, { useState ,useEffect} from 'react';
+import './signup.css';
 
 function Signup() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [job, setJob] = useState('');
+  const [duty, setDuty] = useState('');
+  const [search, setSearch] = useState('');
+  const [school, setSchool] = useState('');
+  const [schoolList, setSchoolList] = useState(['한양대학교','고려대학교','KAIST','숙명여자대학교','부산대학교','POSTEC','UNIST','성균관대학교'])
+  const [filteredSchools, setFilteredSchools] = useState([]);
 
+
+  useEffect(() => {
+    if (search.length === 0) {
+      setFilteredSchools([]);
+    } else {
+      setFilteredSchools(
+        schoolList.filter(school =>
+          school.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    }
+  }, [search, schoolList]);
+
+  const handleSchoolClick = (school) => {
+    setSchool(school);
+    setSearch(school);
+    setFilteredSchools([]);
+  }
   const handleSubmit = (event) => {
     event.preventDefault();
     // 여기서 username과 password를 사용하여 로그인 로직을 처리합니다.
@@ -12,9 +36,90 @@ function Signup() {
     console.log(`Logging in with username: ${username} and password: ${password}`);
   }
 
+  const handleUsernameCheck = () => {
+    console.log(`Checking if username: ${username} is available`);
+    // 여기서 백엔드 API를 호출하여 username이 사용 가능한지 검사합니다.
+    // ...
+  }
+
+  const renderDuties = () => {
+    switch(job) {
+      case 'developer':
+        return (
+          <>
+            <option value="front-end">프론트엔드</option>
+            <option value="back-end">백엔드</option>
+            <option value="full-stack">풀스택</option>
+          </>
+        );
+      case 'designer':
+        return (
+          <>
+            <option value="ui">UI</option>
+            <option value="ux">UX</option>
+            <option value="graphic">그래픽</option>
+          </>
+        );
+      case 'manager':
+        return (
+          <>
+            <option value="product">Product</option>
+            <option value="project">Project</option>
+            <option value="hr">HR</option>
+          </>
+        );
+      default:
+        return ;
+    }
+  }
+
   return (
     <div>
       <h1>Signup Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          ID:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <button onClick={handleUsernameCheck}>중복확인</button>
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <label>
+          직군:
+          <select value={job} onChange={(e) => setJob(e.target.value)}>
+            <option value="">--선택하세요--</option>
+            <option value="developer">개발자</option>
+            <option value="designer">디자이너</option>
+            <option value="manager">매니저</option>
+          
+          </select>
+        </label>
+      
+          <label>
+            직무:
+            <select value={duty} onChange={(e) => setDuty(e.target.value)}>
+              <option value="">--선택하세요--</option>
+              {renderDuties()}
+            </select>
+          </label>
+        
+        <label>
+          학교:
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="학교 이름을 입력하세요"/>
+          <div>
+            {filteredSchools.map((school, index) => (
+              <div key={index} onClick={() => handleSchoolClick(school)}>
+                {school}
+              </div>
+            ))}
+          </div>
+        </label>
+
+        
+        <input type="submit" value="Submit" />
+      </form>
      
     </div>
   );
