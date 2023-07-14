540fbd02a95deae2a308f3f62e59357ed8a70abf
gloveman <gloveman@postech.ac.kr>
2023-07-14 15:35:02 +0900

    login finish

--- my-app/server.js
+++ my-app/server.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const path=require('path')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var app=express();
 var server=require('http').createServer(app);
 app.use(express.json());
@@ -10,8 +11,36 @@ app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'build/index.html'))
 });
 
+app.post('/dologin',async (req,res)=>{
+    try{
+      await client.connect();
+      userdata=client.db('Users').collection('person');
+      const result=await userdata.find(req.body).toArray();
+      if(result.length>0)
+      {
+        //return user info
+        res.json({result:"SUCCESS"});
+      }
+      else
+      //login false
+        res.json({result:"FAIL"});
+    }
+    finally
+    {
+      // client.close();
+    }
+  
+  });
 
 server.listen(80,()=>{
     console.log("Server on 80")
 });
 
+const uri = "mongodb+srv://gloveman50:zohCzGt3lh6icZKl@clustermad.qjzy8y9.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
--- my-app/src/components/login.js
+++ my-app/src/components/login.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react';
+import {Routes,Route } from 'react-router-dom';
 import '../App.css';
+import axios from 'axios';
+import CreateCV from './createCV';
+import { useNavigate } from 'react-router-dom/dist';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [Result,setResult] = useState('');
+  const navigate=useNavigate()
   const handleSubmit = (event) => {
     event.preventDefault();
-    // 여기서 username과 password를 사용하여 로그인 로직을 처리합니다.
-    // 예를 들어 API를 호출하여 로그인을 시도할 수 있습니다.
-    console.log(`Logging in with username: ${username} and password: ${password}`);
+    axios({
+      method:'post',
+      url:'/dologin',
+      data:{
+        'id':username,
+        'password':password
+      }
+    }).then(response=>{
+      if(response.data.result=="SUCCESS")
+      {
+        navigate('/createCV')
+      }
+      else{
+        setResult("Try again")
+      }
+    });
+    //console.log(`Logging in with username: ${username} and password: ${password}`);
   }
 
   return (
@@ -20,12 +39,16 @@ function LoginPage() {
           Username:
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
         </label>
+        <br></br>
         <label>
           Password:
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <div>
+        <h1>{Result}</h1>
+        </div>
     </div>
   );
 }
